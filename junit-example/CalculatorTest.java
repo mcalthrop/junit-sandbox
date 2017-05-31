f5739bd2075329b088a6cf89f320f0b31ac0b504
Matt Calthrop <mcalthrop@gmail.com>
2017-05-31 22:14:38 +0100

    Add @Before to initialise test calculator

--- junit-example/CalculatorTest.java
+++ junit-example/CalculatorTest.java
@@ -1,16 +1,23 @@
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void evaluatesExpressionWithOneNumber() {
-        Calculator calculator = new Calculator();
         int sum = calculator.evaluate("99");
         assertEquals(99, sum);
     }
+
     @Test
     public void evaluatesExpressionWithThreeNumbers() {
-        Calculator calculator = new Calculator();
         int sum = calculator.evaluate("1+2+3");
         assertEquals(6, sum);
     }
