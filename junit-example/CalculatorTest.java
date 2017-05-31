0436729883cf9b8986636322aa8d5a803c1a74a0
Matt Calthrop <mcalthrop@gmail.com>
2017-05-31 21:46:38 +0100

    Add another test: just a single number in the expression

--- junit-example/CalculatorTest.java
+++ junit-example/CalculatorTest.java
@@ -3,7 +3,13 @@ import org.junit.Test;
 
 public class CalculatorTest {
     @Test
-    public void evaluatesExpression() {
+    public void evaluatesExpressionWithOneNumber() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("99");
+        assertEquals(99, sum);
+    }
+    @Test
+    public void evaluatesExpressionWithThreeNumbers() {
         Calculator calculator = new Calculator();
         int sum = calculator.evaluate("1+2+3");
         assertEquals(6, sum);
