b7ff082c6d9df5b8eed4eb0241a410f096c1e751
Matt Calthrop <mcalthrop@gmail.com>
2017-06-01 15:10:43 +0100

    Simplify the bash script

--- step-1/starter-code/run-tests.bash
+++ step-1/starter-code/run-tests.bash
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
-JUNIT_JAR=junit-4.12.jar
-HAMCREST_CORE_JAR=hamcrest-core-1.3.jar
-export CLASSPATH=.:${JUNIT_JAR}:${HAMCREST_CORE_JAR}
+export CLASSPATH=.:junit-4.12.jar:hamcrest-core-1.3.jar
 
 javac Factorial.java FactorialTest.java
 java org.junit.runner.JUnitCore FactorialTest
